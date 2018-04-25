977b1f9183c874c4fce7ec9fe752227139ef55b0
Gabriel Tay <gabrieltay@ip-192-168-1-198.ap-southeast-1.compute.internal>
2018-04-26 02:12:48 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.set('port', (process.env.PORT || 8080));
+
+const secretkey = 'Hello World';
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Welcome to the API'
+    });
+});
+
+app.post('/api/login', genToken, (req, res) => {
+    
+    // Mock User Info
+    const user = {
+        id: 1,
+        username: req.username,
+        access: 'basic user'
+    }
+
+    jwt.sign({user}, secretkey, (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+app.post('/api/post', verifyToken, (req, res) => {
+    jwt.verify(req.token, secretkey, (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: "Auth and Post Successfully",
+                authData
+            });
+        }
+    });
+});
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  
+}
+
+// Generate Token based on User Credentials
+function genToken(req, res, next) {
+    // Get Basic Auth Header value
+    const authHeader = req.headers['authorization'];
+
+    if(typeof authHeader !== 'undefined') {
+        // Split at the space
+        const basic = authHeader.split(' ');
+        // Get Base64 Basic Credentials
+        const cred = Buffer.from(basic[1], 'base64').toString();
+        // Split at the :
+        const credArray = cred.split(':');
+        // Get Username from array
+        req.username = credArray[0];
+        // Get Password from array
+        req.password = credArray[1];
+
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+}
+
+app.listen(app.get('port'), () => console.log('Server started on port ' + app.get('port')));
