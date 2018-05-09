47a2e0fafa4f76d12fc36fbe5913236771fcf0de
Gabriel Tay <gabrieltay@ip-192-168-108-70.ap-southeast-1.compute.internal>
2018-05-09 14:28:18 +0800

    body

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.post('/api/Token',  (req, res) =>  {
         res.json({
             access_token,
             token_type: "bearer",
-            expires_in: 3600
+            expires_in: 3600,
+            req: req
         });
     });
 });
